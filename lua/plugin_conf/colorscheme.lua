1cc57e1b4aec2b79c19086f522227e09ca74e36d
Igor P. Leroy <ip.leroy@gmail.com>
2022-12-14 13:18:19 -0300

    Update UI config

--- lua/plugin_conf/colorscheme.lua
+++ lua/plugin_conf/colorscheme.lua
@@ -1,14 +1,16 @@
 vim.t_Co = 256
 vim.o.termguicolors = true
 
+-- colorscheme
 vim.cmd "colorscheme rosebones"
 vim.opt.background = "light"
 
-
+-- indent column colors
 require("indent_blankline").setup {
   show_current_context = true,
 }
 
+-- Override python icon colorset
 require("nvim-web-devicons").set_icon {
   py = {
     icon = "",
@@ -17,3 +19,10 @@ require("nvim-web-devicons").set_icon {
     name = "Py"
   }
 }
+
+-- Override diagnostic signs
+local signs = { Error = " ", Warn = " ", Hint = " ", Info = " " }
+for type, icon in pairs(signs) do
+  local hl = "DiagnosticSign" .. type
+  vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
+end
