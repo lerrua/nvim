dda8370c18d0c83b620ab984ab9cdfe6b1095d6c
Igor P. Leroy <ip.leroy@gmail.com>
2022-12-14 17:14:24 -0300

    LSP configurations

--- lua/plugin_conf/lsp_config.lua
+++ lua/plugin_conf/lsp_config.lua
@@ -3,9 +3,9 @@ require("mason-lspconfig").setup({
   ensure_installed = { "sumneko_lua", "solargraph", "pyright", "tsserver" }
 })
 
-local on_attach = function(_, bufnr)
-  vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
+local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
+local on_attach = function(_, _)
   vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, {})
   vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, {})
 
@@ -17,17 +17,76 @@ end
 
 require("lspconfig")['sumneko_lua'].setup {
   on_attach = on_attach,
+  capabilities = capabilities,
 }
 
 require("lspconfig")['solargraph'].setup {
-  on_attach = on_attach
+  on_attach = on_attach,
+  capabilities = capabilities,
 }
 
 require("lspconfig")['pyright'].setup {
-  on_attach = on_attach
+  on_attach = on_attach,
+  capabilities = capabilities,
 }
 
 require("lspconfig")['tsserver'].setup {
   on_attach = on_attach,
+  capabilities = capabilities,
   filetypes = { "javascript", "typescript", "typescriptreact", "typescript.tsx" }
+}
+
+-- lspkind setup
+local lspkind = require('lspkind')
+
+-- luasnip setup
+require("luasnip.loaders.from_vscode").lazy_load()
+local luasnip = require('luasnip')
+
+-- nvim-cmp setup
+local cmp = require('cmp')
+cmp.setup {
+  snippet = {
+    expand = function(args)
+      luasnip.lsp_expand(args.body)
+    end,
+  },
+  mapping = cmp.mapping.preset.insert({
+    ['<C-d>'] = cmp.mapping.scroll_docs(-4),
+    ['<C-f>'] = cmp.mapping.scroll_docs(4),
+    ['<C-Space>'] = cmp.mapping.complete(),
+    ['<CR>'] = cmp.mapping.confirm {
+      behavior = cmp.ConfirmBehavior.Replace,
+      select = true,
+    },
+    ['<Tab>'] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_next_item()
+      elseif luasnip.expand_or_jumpable() then
+        luasnip.expand_or_jump()
+      else
+        fallback()
+      end
+    end, { 'i', 's' }),
+    ['<S-Tab>'] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_prev_item()
+      elseif luasnip.jumpable(-1) then
+        luasnip.jump(-1)
+      else
+        fallback()
+      end
+    end, { 'i', 's' }),
+  }),
+  sources = {
+    { name = 'nvim_lsp' },
+    { name = 'luasnip' },
+  },
+  formatting = {
+    format = lspkind.cmp_format({
+      mode = 'symbol',
+      maxwidth = 50,
+      ellipsis_char = '...',
+    })
+  }
 }
