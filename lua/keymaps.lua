e7d3cff59929e2a4ef9d2b2db2a6969b29b75b66
Igor P. Leroy <ip.leroy@gmail.com>
2022-12-27 11:34:16 -0300

    Update keymaps

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -4,6 +4,4 @@ vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
 vim.keymap.set("x", "<leader>p", [["_dP]])
-
 vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
-vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
