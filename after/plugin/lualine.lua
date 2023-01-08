49207c032e76dbf6c7752e3481097e21898f5e4c
Igor P. Leroy <ip.leroy@gmail.com>
2023-01-08 09:54:24 -0300

    Update lualine

--- after/plugin/lualine.lua
+++ after/plugin/lualine.lua
@@ -34,6 +34,7 @@ end
 
 require('lualine').setup {
   options = {
+    -- theme = 'tokyonight',
     icons_enabled = true,
     component_separators = '',
     section_separators = { left = '', right = '' },
@@ -41,7 +42,8 @@ require('lualine').setup {
   },
   sections = {
     lualine_a = { custom_vim_mode },
-    lualine_c = {},
+    lualine_b = {},
+    lualine_c = {'branch', 'diff', 'diagnostics'},
     lualine_x = {'filetype'},
     lualine_y = {},
   }
