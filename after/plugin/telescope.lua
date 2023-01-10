087234fab6783467a4d1502b34c83320d986f5ef
Igor P. Leroy <ip.leroy@gmail.com>
2023-01-10 08:52:49 -0300

    Better keymaps

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -11,11 +11,13 @@ require('telescope').load_extension('fzf')
 require('telescope').load_extension('file_browser')
 
 -- keymaps
+local opts = { remap = false, silent = true }
 local builtin = require('telescope.builtin')
+
 vim.keymap.set('n', '<space>e', builtin.find_files, {})
 vim.keymap.set('n', '<space><space>', builtin.oldfiles, {})
 vim.keymap.set('n', '<space>f', builtin.live_grep, {})
 vim.keymap.set('n', '<space>fg', builtin.grep_string, { desc = '[S]earch current [W]ord' })
 vim.keymap.set('n', '<space>b', builtin.buffers, {})
 vim.keymap.set('n', '<space>t', builtin.help_tags, {})
-vim.keymap.set("n", "<F3>", ":Telescope file_browser<CR>", { noremap = true })
+vim.keymap.set("n", "<F3>", ":Telescope file_browser<CR>", opts)
--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -1,22 +1,24 @@
-vim.keymap.set('n', '<leader><space>', ':nohlsearch<CR>')
+local opts = { remap = false, silent = true }
 
-vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
-vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+vim.keymap.set('n', '<leader><space>', ':nohlsearch<CR>', opts)
 
-vim.keymap.set("x", "<leader>p", [["_dP]])
-vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv", opts)
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv", opts)
+
+vim.keymap.set("x", "<leader>p", [["_dP]], opts)
+vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]], opts)
 
 -- Set working directory
-vim.keymap.set("n", "<leader>.", ":cd %:p:h<CR>:pwd<CR>")
+vim.keymap.set("n", "<leader>.", ":cd %:p:h<CR>:pwd<CR>", opts)
 
 -- Tabs navigation
-vim.keymap.set('n', '<Tab>', 'gt')
-vim.keymap.set('n', '<S-Tab>', 'gT')
-vim.keymap.set('n', '<S-t>', ':tabnew<CR>')
-vim.keymap.set('n', '<leader>z', ':tabnew %<CR>')
+vim.keymap.set('n', '<Tab>', 'gt', opts)
+vim.keymap.set('n', '<S-Tab>', 'gT', opts)
+vim.keymap.set('n', '<S-t>', ':tabnew<CR>', opts)
+vim.keymap.set('n', '<leader>z', ':tabnew %<CR>', opts)
 
 -- Buffers navigation
-vim.keymap.set('n', '<BS>', ':bprevious<CR>')
+vim.keymap.set('n', '<BS>', ':bprevious<CR>', opts)
 
-vim.keymap.set('n', 'q', ':q<CR>')
-vim.keymap.set('n', '<leader>w', ':w<CR>')
+vim.keymap.set('n', 'q', ':q<CR>', opts)
+vim.keymap.set('n', '<leader>w', ':w<CR>', opts)
