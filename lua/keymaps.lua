b245c4a603f4d2dfa26ad6b5e78855b8b20179e0
Igor P. Leroy <ip.leroy@gmail.com>
2023-01-26 18:27:12 -0300

    Update keymaps

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -1,6 +1,6 @@
 local opts = { remap = false, silent = true }
 
-vim.keymap.set('n', '<leader><space>', ':nohlsearch<CR>', opts)
+vim.keymap.set('n', '<leader><space>', '<CMD>nohlsearch<CR>', opts)
 
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv", opts)
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv", opts)
@@ -9,20 +9,20 @@ vim.keymap.set("x", "<leader>p", [["_dP]], opts)
 vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]], opts)
 
 -- Set working directory
-vim.keymap.set("n", "<leader>.", ":cd %:p:h<CR>:pwd<CR>", opts)
+vim.keymap.set("n", "<leader>.", "<>cd %:p:h<CR>:pwd<CR>", opts)
 
 -- Tabs navigation
 vim.keymap.set('n', '<Tab>', 'gt', opts)
 vim.keymap.set('n', '<S-Tab>', 'gT', opts)
-vim.keymap.set('n', '<S-t>', ':tabnew<CR>', opts)
-vim.keymap.set('n', '<leader>z', ':tabnew %<CR>', opts)
+vim.keymap.set('n', '<S-t>', '<CMD>tabnew<CR>', opts)
+vim.keymap.set('n', '<leader>z', '<CMD>tabnew %<CR>', opts)
 
 -- Window navigation
-vim.keymap.set('n', '<leader>h', ':sp<CR>', opts)
-vim.keymap.set('n', '<leader>v', ':vsp<CR>', opts)
+vim.keymap.set('n', '<leader>h', '<CMD>sp<CR>', opts)
+vim.keymap.set('n', '<leader>v', '<CMD>vsp<CR>', opts)
 
 -- Buffers navigation
-vim.keymap.set('n', '<BS>', ':bprevious<CR>', opts)
+vim.keymap.set('n', '<BS>', '<CMD>bprevious<CR>', opts)
 
-vim.keymap.set('n', 'q', ':q<CR>', opts)
-vim.keymap.set('n', '<leader>w', ':w<CR>', opts)
+vim.keymap.set('n', 'q', '<CMD>q<CR>', opts)
+vim.keymap.set('n', '<leader>w', '<CMD>write<CR>', opts)
