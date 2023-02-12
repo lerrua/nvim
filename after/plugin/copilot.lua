ce5eb2f6a65b0fef44b06a933077e2853d618ebf
Igor P. Leroy <ip.leroy@gmail.com>
2023-02-12 19:16:31 -0300

    Fix copilot

--- after/plugin/copilot.lua
+++ after/plugin/copilot.lua
@@ -3,5 +3,6 @@ vim.g.copilot_filetypes = {
 }
 vim.g.copilot_no_tab_map = true
 
-vim.keymap.set("i", "<C-a>", "copilot#Accept()", { script = true, expr = true, remap = false, silent = true })
+vim.keymap.set("i", "<C-a>", "copilot#Accept()",
+  { script = true, expr = true, remap = false, silent = true, replace_keycodes = false })
 vim.keymap.set("i", "<C-x>", "copilot#Dismiss()", { script = true, expr = true, remap = false, silent = true })
