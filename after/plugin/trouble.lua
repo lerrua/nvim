d6f6fe41fa34b97f286d6b3ffe9786d84f02ed05
Igor P. Leroy <ip.leroy@gmail.com>
2023-02-12 19:16:55 -0300

    F10 map to trouble-plugin

--- after/plugin/trouble.lua
+++ after/plugin/trouble.lua
@@ -7,3 +7,7 @@ for type, icon in pairs(signs) do
   local hl = "DiagnosticSign" .. type
   vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
 end
+
+
+local opts = { remap = false, silent = true }
+vim.keymap.set('n', '<F10>', '<CMD>TroubleToggle<CR>', opts)
