db5cb1b21113b7167db9d0a17ac0ebca9b3531e9
Igor P. Leroy <ip.leroy@gmail.com>
2025-05-26 01:27:04 -0300

    Rename config to configs

--- init.lua
+++ init.lua
@@ -1,9 +1,9 @@
 -- Set options and keymaps before loading plugins
-require("config.options")
+require("configs.options")
 require("config.keymaps")
 
 -- Initialize lazy.nvim and load plugins
-require("config.lazy-setup")
+require("configs.lazy-setup")
 
 -- Set others configurations
-require("config.diagnostics")
+require("configs.diagnostics")
