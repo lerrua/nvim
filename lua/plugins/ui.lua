ef5a7869b4ca9eba4dea5c3b0d6602a0dd5185c4
Igor P. Leroy <ip.leroy@gmail.com>
2025-05-31 08:13:37 -0300

    UI tweaks

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -53,6 +53,7 @@ local function custom_filetype()
     lazygit = "󰊢 LazyGit",
     help = "󰋖 Help Page",
     AvanteInput = "󱙺 Avante AI",
+    AvantePromptInput = "󱙺 Avante AI",
     Outline = "󰈔 Code Symbols",
     qf = "󰅚 Quickfix",
     diff = "󰀨 Diff",
@@ -60,7 +61,7 @@ local function custom_filetype()
     trouble = " Trouble",
     spectre_panel = "󱁴 Spectre Panel",
     toggleterm = " Floating Terminal",
-    fugitiveblame = "󰊢 Git Blame",
+    ["gitsigns-blame"] = "󰊢 Git Blame",
   }
 
   if filetype_icon[filetype] ~= nil then
@@ -258,11 +259,11 @@ return {
           },
         },
         presets = {
-          bottom_search = true, -- use a classic bottom cmdline for search
-          command_palette = true, -- position the cmdline and popupmenu together
+          bottom_search = true,         -- use a classic bottom cmdline for search
+          command_palette = true,       -- position the cmdline and popupmenu together
           long_message_to_split = true, -- long messages will be sent to a split
-          inc_rename = false, -- enables an input dialog for inc-rename.nvim
-          lsp_doc_border = true, -- add a border to hover docs and signature help
+          inc_rename = false,           -- enables an input dialog for inc-rename.nvim
+          lsp_doc_border = true,        -- add a border to hover docs and signature help
         },
       })
     end,
