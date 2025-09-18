04c11756edb1ea7911cbea950d7cc951bf4afc45
Igor P. Leroy <ip.leroy@gmail.com>
2025-09-18 18:49:57 -0300

    Fix catppucin bufferline

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -106,7 +106,7 @@ return {
     dependencies = { "nvim-tree/nvim-web-devicons" },
     config = function()
       require("bufferline").setup({
-        highlights = require("catppuccin.groups.integrations.bufferline").get(),
+        highlights = require("catppuccin.groups.integrations.bufferline").get_theme(),
         options = {
           color_icons = false,
           separator_style = "slant",
@@ -287,11 +287,11 @@ return {
           },
         },
         presets = {
-          bottom_search = true,         -- use a classic bottom cmdline for search
-          command_palette = true,       -- position the cmdline and popupmenu together
+          bottom_search = true, -- use a classic bottom cmdline for search
+          command_palette = true, -- position the cmdline and popupmenu together
           long_message_to_split = true, -- long messages will be sent to a split
-          inc_rename = false,           -- enables an input dialog for inc-rename.nvim
-          lsp_doc_border = true,        -- add a border to hover docs and signature help
+          inc_rename = false, -- enables an input dialog for inc-rename.nvim
+          lsp_doc_border = true, -- add a border to hover docs and signature help
         },
       })
     end,
